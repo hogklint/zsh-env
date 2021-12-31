c9569de4ca345fc8534e64123177bde7f9e3ed33
Jimmie Högklint <hogklint@gmail.com>
2021-12-31 18:04:53 +0100

    Various crown additions

--- profiles/crown.zsh
+++ profiles/crown.zsh
@@ -1,7 +1,51 @@
 export prompt_color="$fg_brown"
 export TERM=dtterm
 
+source ~/.dk_variables
+
+# make sh
+export ITFLAG=-it
+
+# Make gpg-agent work with SSH keys
+export GPG_TTY=$(tty)
+#echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
+
+# pass shift-insert clipboard
+export PASSWORD_STORE_X_SELECTION=primary
+
+# Perl (Sqitch)
+PATH="/home/jhogklint/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/home/jhogklint/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/home/jhogklint/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/home/jhogklint/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/home/jhogklint/perl5"; export PERL_MM_OPT;
+
+
+#######
+# Alias/Functions
+#
+
+alias vo='vim ~/repos/orgmode/main.org'
+alias p="cd $PLATFORM_HOME"
+alias aga='ag --ignore tests'
+alias kc='kubectl'
+
+function pullpr()
+{
+  for pr in "$@"
+  do
+    if git rev-parse --verify "pr$pr"
+    then
+      git checkout master
+      git branch -D "pr$pr"
+    fi
+    git fetch origin "pull/$pr/head:pr$pr"
+    git checkout "pr$pr"
+  done
+}
+
+# Start X
 if [[ -z "$DISPLAY" && $(tty) == /dev/tty1 ]]
 then
-    startx
+    exec startx
 fi
